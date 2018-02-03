fa1297435a81035688b751a0b88995564602bc61
SillyGG <137100856@163.com>
2018-02-03 10:43:42 +0800

    第二次提交,新增页面样式

--- DjangoGetStarted/settings.py
+++ DjangoGetStarted/settings.py
@@ -74,8 +74,11 @@ WSGI_APPLICATION = 'DjangoGetStarted.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testdjango',
+        'USER': 'root',
+        'PASSWORD': '123123',
+        'HOST': '127.0.0.1'
     }
 }
 
@@ -114,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
